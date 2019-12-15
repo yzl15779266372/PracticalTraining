37bac6e1876fcdfe092a67394a71a856814e6387
yzl_bal <2623099802@qq.com>
2019-12-15 19:56:27 +0800

    Second Commit

---
+++ src/test/TestProject.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.Test;
+
+/**
+ * Created by shkstart on 2019/12/15
+ */
+public class TestProject {
+
+    @Test
+    public void testProject(){
+        System.out.println("success");
+    }
+}
