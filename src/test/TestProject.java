9941c9e4b3cc20d5a147dc481ae1439d6e60c785
yzl_bal <2623099802@qq.com>
2019-12-16 08:20:26 +0800

    Second Commit

--- src/test/TestProject.java
+++ src/test/TestProject.java
@@ -1,6 +1,10 @@
 package test;
 
+import com.prac.db.JdbcUtils;
 import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * Created by shkstart on 2019/12/15
@@ -11,5 +15,11 @@ public class TestProject {
     public void testProject(){
         System.out.println("success");
         System.out.println("成功");
+    }
+
+    @Test
+    public void testConnection() throws SQLException {
+        Connection conn = JdbcUtils.getConnection();
+        System.out.println(conn);
     }
 }
